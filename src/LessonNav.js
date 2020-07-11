e431e3795f78ac9994d8efc51fe89c68f9ab0bec
raymondgao <raymond.gao@wizeline.com>
2020-07-11 11:12:28 +0700

    fix gui

--- src/LessonNav.js
+++ src/LessonNav.js
@@ -13,8 +13,29 @@ export default class CameraNav extends React.Component {
         return (
             
             <Stack.Navigator>
-                <Stack.Screen name="ListLesson" component={ListLesson} />
-                <Stack.Screen name="LearnResources" component={LearnResources} />
+                <Stack.Screen name="ListLesson" component={ListLesson}  options={{
+                    title: 'Show Lessons',
+                    headerStyle: {
+                        height: 25,
+                      backgroundColor: '#2196F3',
+                    },
+                    headerTintColor: '#fff',
+                    headerTitleStyle: {
+                        fontSize: 16,
+                    },
+                  }} />
+                <Stack.Screen name="LearnResources" component={LearnResources}
+                 options={{
+                    title:  'Learn Lessons',
+                    headerStyle: {
+                        height: 25,
+                      backgroundColor: '#2196F3',
+                    },
+                    headerTintColor: '#fff',
+                    headerTitleStyle: {
+                        fontSize: 16,
+                    },
+                  }} />
             </Stack.Navigator>
             
         );
